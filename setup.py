61c9b3b4d588161a2e6549cdc950c3efa6a76d50
mattijn <mattijn@gmail.com>
2017-10-10 09:59:55 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+setup(name='hkvfewspy',
+      version='0.2.2',
+      description='HKV tools voor operationeel waterbeheer',
+      author='Mattijn van Hoek',
+      author_email='mattijn.vanhoek@hkv.nl',
+      packages=find_packages(),
+      install_requires=[
+          'zeep',
+          'pytz',
+          'numpy',
+          'pandas'
+      ],
+      dependency_links=[
+        'http://www.lfd.uci.edu/~gohlke/pythonlibs/#fiona',
+        'http://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal',
+        'http://geopandas.org/install.html'
+        ]
+     )
