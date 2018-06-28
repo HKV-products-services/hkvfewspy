139d25a1b081ef1b0d82bfb1fb2b9b687e12f3c9
Raoul Collenteur <info@raoulcollenteur.nl>
2018-06-28 12:01:03 +0200

    Revert "Fix package dependencies and optimize import"
    This reverts commit 58d3077bcefead6001dd334f04fe0412a4932bb9.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-# from distutils.core import setup
+#from distutils.core import setup
 from setuptools import setup, find_packages
-
 setup(name='hkvfewspy',
       version='0.3.1',
       description='HKV tools voor operationeel waterbeheer',
@@ -10,17 +9,14 @@ setup(name='hkvfewspy',
       author_email='mattijn.vanhoek@hkv.nl',
       packages=find_packages(),
       install_requires=[
-          'zeep>=3.0.0',
+          'zeep',
           'pytz',
           'numpy',
-          'pandas',
-          'geopandas',
-          'shapely',
-          'fire'
+          'pandas'
       ],
       dependency_links=[
-          'http://www.lfd.uci.edu/~gohlke/pythonlibs/#fiona',
-          'http://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal',
-          'http://geopandas.org/install.html'
-      ]
-      )
+        'http://www.lfd.uci.edu/~gohlke/pythonlibs/#fiona',
+        'http://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal',
+        'http://geopandas.org/install.html'
+        ]
+     )
