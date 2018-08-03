ceb60656616dffe1f4a9b0a367dfd53763762bcb
mattijn <mattijn@gmail.com>
2018-08-03 22:34:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hkvfewspy',
-      version='0.5.6',
+      version='0.5.7',
       description='HKV tools voor operationeel waterbeheer',
       author='Mattijn van Hoek',
       author_email='mattijn.vanhoek@hkv.nl',
