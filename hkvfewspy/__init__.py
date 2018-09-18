7fa81731c78903f7e80317a09cbe10a1cecde234
mattijn <mattijn@gmail.com>
2018-09-18 21:17:14 +0200

    Update __init__.py

--- hkvfewspy/__init__.py
+++ hkvfewspy/__init__.py
@@ -1,10 +1,9 @@
-from .io.soap_fewspi import pi
+from .io.soap_fewspi import Pi
 from .timeseries import FewsTimeSeries, FewsTimeSeriesCollection
 
 __doc__ = """package for accessing fewspi service"""
-__version__ = '0.6.0'
+__version__ = '0.6.0dev'
 
-pi = pi()
 
 if __name__ == '__main__':
     try:
