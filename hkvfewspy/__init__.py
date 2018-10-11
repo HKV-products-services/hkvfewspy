d6d8829d5e4a075ed49213da70bf0129e8eb5137
Hoek <hoek@hkv.nl>
2018-10-11 12:09:08 +0200

    add __init__.py in schemas

--- hkvfewspy/__init__.py
+++ hkvfewspy/__init__.py
@@ -1,9 +1,9 @@
 from .io.soap_fewspi import PiSoap 
-from .schemas.timeseries import FewsTimeSeries, FewsTimeSeriesCollection
+#from .schemas.timeseries import FewsTimeSeries, FewsTimeSeriesCollection
 
 # currently PiSoap maps to Pi
 # in future user should decide which webservice to use
 Pi = PiSoap
 
 __doc__ = """package for accessing fewspi service"""
-__version__ = '0.6.1dev'
+__version__ = '0.6.1'
